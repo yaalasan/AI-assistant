6e965f8991850ee2f0348ceac982f6dd6d65a77f
Senior Yhlas <rahmaniyazovy@gmail.com>
2025-10-22 20:39:51 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# web interface
-import os
-import gradio as gr
+# backend
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -12,6 +10,7 @@ from transformers import pipeline
 def build_qa(pdf_path):
     loader = PyPDFLoader(pdf_path)
     docs = loader.load()
+
     splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     chunks = splitter.split_documents(docs)
 
@@ -19,11 +18,6 @@ def build_qa(pdf_path):
     #create FAISS db
     db = FAISS.from_documents(chunks, embed)
 
-    #Ensure data folder exists
-    os.makedirs("data", exist_ok=True)
-
-    #Save FAISS index
-    db.save_local("data/faiss_index")
 
     #Load LLM
     hf_pipe = pipeline(
@@ -35,47 +29,10 @@ def build_qa(pdf_path):
 
     qa = RetrievalQA.from_chain_type(
         llm=llm,
-        retriever=db.as_retriever(search_kwargs={"k": 5})
+        retriever=db.as_retriever(search_kwargs={"k": 4})
     )
     return qa
 
 
-qa_holder = {"qa": None}
 
-def upload_pdf(pdf_file):
-    qa_holder["qa"] = build_qa(pdf_file.name)
-    return "PDF loaded! You can start asking questions."
-
-def ask_questions(question):
-    if qa_holder["qa"] is None:
-        return "Please upload a PDF first."
-    result = qa_holder["qa"].invoke(question)
-    return result["result"]
-
-#Gradio UI
-def create_interface():
-    with gr.Blocks(title="Mini AI PDF Assistant") as demo:
-        gr.Markdown("# Mini AI Assistant\nChat with any PDF you upload.")
-
-# upload + status section
-
-        pdf_file = gr.File(label="Upload a PDF")
-        status_box = gr.Textbox(label="Status", interactive=False)
-
-#chat section
-        question_box = gr.Textbox(label="Ask a question about your PDF")
-        answer_box = gr.Textbox(label="Answer", interactive=False)
-
-#Event buildings
-        pdf_file.upload(upload_pdf, inputs=pdf_file, outputs=status_box)
-        question_box.submit(ask_questions, inputs=question_box, outputs=answer_box)
-
-
-
-    return demo
-
-#launch
-if __name__ == "__main__":
-    demo = create_interface()
-    demo.launch()
 
